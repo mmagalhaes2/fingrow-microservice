3f0d0f61fe7896288d291092f187bb10fe229af3
Matheus Magalhaes <mathhm2@gmail.com>
2024-05-21 19:33:36 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/fingrow/FingrowApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fingrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingrowApplication.class, args);
+    }
+
+}
